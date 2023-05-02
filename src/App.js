fa1ae93eafe279951d39ab3902eff2919a3617c7
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-05-02 09:32:44 +0900

    for_blog

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
-import './App.css';
+import "./App.css";
 
-const fruit = [
-  {
-    name : "apple",
-    color : 'red',
-    price : "120",
-  },
-  {
-    name: "banana",
-    color: 'yellow',
-    price: "200",
-  },
-  {
-    name: "melon",
-    color: 'green',
-    price: "900",
-  },
-]
+const array = [1, 2, 3, 4, 5];
 
-
-function App() {
+const App = () => {
   return (
-  <div>
-    {fruit.map(fruit => {
-      return (
-        <h2>fruit.name<h2/>
-        <p>fruit.color<p/>
-        <p>fruit.price<p/>
-      )
-    }) 
-
-    }
-    
-  </div>
+    <div>
+      {array.map((item) => {
+        return <li key={item}>{item}</li>;
+      })}
+    </div>
   );
-}
+};
 
 export default App;
+// const fruit = [
+//   {
+//     id: 1,
+//     name: "apple",
+//     color: "red",
+//     price: "120",
+//   },
+//   {
+//     id: 2,
+//     name: "banana",
+//     color: "yellow",
+//     price: "200",
+//   },
+//   {
+//     id: 3,
+//     name: "melon",
+//     color: "green",
+//     price: "900",
+//   },
+// ];
+
+// function App() {
+//   return (
+//     <div className="container" key={fruit.id}>
+//       {fruit.map((fruit) => {
+//         return (
+//           <div className="fruit" key={fruit.id}>
+//             <h2>{fruit.name}</h2>
+//             <p>{fruit.color}</p>
+//             <p>{fruit.price}</p>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/hoge.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const hoge = () => {
+  return <div>hoge</div>;
+};
+
+export default hoge;
